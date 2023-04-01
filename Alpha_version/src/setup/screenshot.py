fb9dfbe124c645e8ada299a86ba8a44cff20dbad
FridaWang <379788704@qq.com>
2023-04-01 14:11:30 -0400

    create plugin v2

--- Alpha_version/src/setup/screenshot.py
+++ Alpha_version/src/setup/screenshot.py
@@ -1,17 +1,18 @@
 import pygetwindow as gw
 import pyautogui
 
-# Find the window you want to capture by its title (replace 'Window Title' with the actual title)
-window = gw.getWindowsWithTitle('Procedural Knots')[0]
+def takeScreenshot():
+    # Find the window you want to capture by its title (replace 'Window Title' with the actual title)
+    window = gw.getWindowsWithTitle('Procedural Knots')[0]
 
-# Activate and bring the window to the foreground
-window.activate()
+    # Activate and bring the window to the foreground
+    window.activate()
 
-# Get the window's dimensions (x, y, width, height)
-x, y, width, height = window.left+400, window.top+400, window.width-720, window.height-720
+    # Get the window's dimensions (x, y, width, height)
+    x, y, width, height = window.left+400, window.top+400, window.width-720, window.height-720
 
-# Capture the screenshot
-screenshot = pyautogui.screenshot(region=(x, y, width, height))
+    # Capture the screenshot
+    screenshot = pyautogui.screenshot(region=(x, y, width, height))
 
-# Save the screenshot to a file
-screenshot.save('screenshot.png')
+    # Save the screenshot to a file
+    screenshot.save('screenshot.png')
